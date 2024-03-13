6b8aaa2cb67990fab0064a584d9d048d12dc992d
JohnnyIdahoan <121368802+Christensen-John@users.noreply.github.com>
2024-03-13 13:24:51 +0900

    Added sorting functionality to TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -50,6 +50,63 @@ export class TodoList {
     );
   }
 
+  sortList(property = "dateCreated") {
+    //Check if list has elements
+    try {
+      if (this.list.length === 0) {
+        throw "Empty List";
+      }
+    } catch (err) {
+      console.alert(`Sorting Error: ${err}`);
+    } finally {
+      //Save a new list sorted based on the chosen property
+      let newList = this.#sort(this.list, property);
+      //Use the setter to update the list
+      this.list = newList;
+      //Return the newly created list
+      return this.list;
+    }
+
+    //TODO: Try the array.sort(compareFn(a, b)) function
+  }
+
+  #sort(list, property) {
+    //Save the smallest item in the list and pop it from the array
+    //Recursively call this.#sort with the remainder of the list
+    //Return the scoped smallest combined with the rest of the array
+
+    //base case if length of list === 1
+    if (list.length === 1) {
+      return list;
+    }
+
+    //Get smallest item's index
+    let indexOfSmallest = this.#indexOfSmallestValue(list, property);
+
+    //Get smallest item and list is shortened
+    let item = list.splice(indexOfSmallest, 1)[0];
+
+    //Sort the remainder of list recursively
+    let sortedArray = this.#sort(list, property);
+
+    //Combine the smallest item with the now sorted remainder
+    sortedArray.unshift(item);
+
+    //Return the now sorted array
+    return sortedArray;
+  }
+
+  // Find smallest index for insertion sort algorithm
+  #indexOfSmallestValue(list, prop) {
+    let smallestIndex = 0;
+    list.forEach((currentElement, index) => {
+      if (currentElement[prop] < list[smallestIndex][prop]) {
+        smallestIndex = index;
+      }
+    });
+    return smallestIndex;
+  }
+
   //Accessors
   get fullList() {
     return {
